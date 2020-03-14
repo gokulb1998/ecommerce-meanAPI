8ea648c1f34d5b5eb522be91b17fcfdbf0bed893
gokulb1998 <b.gokul100@gmail.com>
2020-03-14 13:31:37 +0530

    updated

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ app.use(cors());
 
 // Launch app to listen to specified port
 app.listen(port, function () {
-    console.log("Running RestHub on port " + port);
+    console.log("Running on port " + port);
 });
